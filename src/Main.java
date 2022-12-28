f09cb64fadade792651245028e8e5c47319ce0ee
denisd79 <89902772+denisd79@users.noreply.github.com>
2022-12-28 14:46:18 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 10; i <= 10 & i >= -10; i-- )
+            System.out.println("Hello world!" + i);
+        {
+            System.out.println("**********************");
+            for (int a = 1904; a <= 2096; a = a + 4)
+                System.out.println("Задание 2" + a);
+        }
     }
 }
