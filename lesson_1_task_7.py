1bcd0ba1b1e8dd51dea85027f1631c23717d776a
denisd79 <89902772+denisd79@users.noreply.github.com>
2023-05-27 17:36:42 +0300

    Task 7

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,32 +1,22 @@
 def fun1 ():
-    print("1")
-    return 1
+    print(1, end="")  
 def fun2 ():
-    print(2)
-    return 2
+    print(2, end="")
 def fun3 ():
-    print(3)
-    return 3
+    print(3, end="")
 def fun4 ():
-    print(4)
-    return 4
+    print(4, end="")
 def fun5 ():
-    print(5)
-    return 5
+    print(5, end="")
 def fun6 ():
-    print(6)
-    return 6
+    print(6, end="")
 def fun7 ():
-    print(7)
-    return 7
+    print(7, end="")
 def fun8 ():
-    print(8)
-    return 8
+    print(8, end="")
 def fun9 ():
-    print("9")
-    return 9
+    print(9, end="")
 def fun0 ():
-    print("0")
-    return 0
-fun8()+fun8(),fun0(),fun0(),fun5(),fun5(),fun5(),fun3(),fun5(),fun3(),fun5()
+    print(0, end="")
+fun8(),fun8(),fun0(),fun0(),fun5(),fun5(),fun5(),fun3(),fun5(),fun3(),fun5()
 
