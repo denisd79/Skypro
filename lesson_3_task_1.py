d3d393361579eb7f54a73178b51a9c7548ebba90
Denis <89902772+denisd79@users.noreply.github.com>
2023-08-30 10:45:31 +0300

    Update lesson_3_task_1.py

---
+++ lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+my_user = User("Имя1", "Иванов")
+
+# Вызываем методы у объекта в переменной my_user
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
