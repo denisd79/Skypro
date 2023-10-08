631a9a0f5344bd813abe2b614b1df2a460ca6ee7
Denis <89902772+denisd79@users.noreply.github.com>
2023-10-08 11:41:44 +0300

    Update lesson_3_task_4.py

--- Lesson 3/lesson_3_task_4.py
+++ Lesson 3/lesson_3_task_4.py
@@ -6,18 +6,15 @@ window.bgcolor('skyblue')
 my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 
-# Нарисуем тело рыбы
 my_turtle.color("yellow")
 my_turtle.begin_fill()
 my_turtle.circle(75)
 my_turtle.end_fill()
 
-# Переместим черепаху в позицию под плавником
 my_turtle.penup()
 my_turtle.goto(-50, -100)
 my_turtle.pendown()
 
-# Нарисуем хвост рыбы
 my_turtle.color("yellow")
 my_turtle.begin_fill()
 my_turtle.right(150)
